e98f9e983b4213ea35f6cc4ce0da53e6f3e32352
Lukasz <lukasz.kurasinski@gmail.com>
2018-11-17 15:32:44 +0100

    new

---
+++ src/main/AlgosMain.java
@@ -0,0 +1,23 @@
+package main;
+
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class AlgosMain extends JPanel{
+
+	
+	public void paint(Graphics g) {
+		super.paint(g);
+	}
+	
+	public void update(Graphics g) {
+		paint(g);
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		
+	}
+}
